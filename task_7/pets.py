1acf756273a0fd608048023201ccd85949af61f0
Velinghton <r.velinghton@gmail.com>
2023-04-22 14:57:43 +0300

    seconf commit: apgrade cod

--- task_7/pets.py
+++ task_7/pets.py
@@ -1,28 +1,66 @@
 class Person:
-    ...
-
-class Owner(Person):
-    def __init__(self, *args):
+    def __init__(self,name, *args):
+        self.name = name
         self.pets = args
-
+class Owner(Person):
+    ...
 class OwnedPets(Owner):
-    
+
+    def show_pets(self):
+        return self.__dict__
+    def add_new_pet(self):
+        list_of_pets.append(self.__dict__)
+        return list_of_pets
+    def union_pets(*args):
+        res = {}
+        for i in args:
+            for k, v in i.__dict__.items():
+                res[k]=res.get(k, []) + [v]
+        return res
+
+
 
 class Animal:
-    def __init__(self, name, age, color):
-        self.name = name
+    names = []
+    def __init__(self, pets, age, color):
+        self.pets = pets
         self.age = age
         self.color = color
+        self.names.append(pets)
 
-
-class Pet(Animal):
+class Pet(Animal, OwnedPets):
     ...
 
-class Cat(Animal):
+class Cat(Pet):
+    ...
+class Dog(Pet):
+    ...
+class Cow(Pet):
     ...
 
-class Dog(Animal):
-    ...
+if __name__ == '__main__':
+    list_of_pets = []
+    #відображення домашніх улюбленців що є у власника
+    bob = OwnedPets("Bob", "cat", "dog", "cow")
+    liza = OwnedPets("Liza", "rabbit", "mouse", "cow")
+    for i in(bob, liza):
+        print(f'{i.name} має таких тварин: {i.pets}')
+        print(i.show_pets())
+        print()
 
-class Cow(Animal):
-    ...
+    # Обєднання тварин
+    print(f'Обєднання тварин двох власників: \n{OwnedPets.union_pets(bob, liza)}')
+    print()
+
+    #Додавання нової тварини
+    cat = Cat('cat', 55, 'red')
+    dog = Dog("dog", 100, 'black')
+    cow = Cow("cow", 77, 'green')
+    cow1 = Cow("cow1", 40, 'yellow')
+    cow2 = Cow("cow2", 10, "brown")
+    for i in (cat, dog, cow, cow1):
+        i.add_new_pet()
+    print(f'Список доданих тварин: {list_of_pets}')
+    print(f"Список доданих тварин: {cat.names}")
+    print()
+
